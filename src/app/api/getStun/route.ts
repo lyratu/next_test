03eadf8c87afde69a5c1c1d2271149e3fb5152cb
akko <1421193732@qq.com>
2025-01-22 01:59:56 +0800

    fixed

--- src/app/api/getStun/route.ts
+++ src/app/api/getStun/route.ts
@@ -2,7 +2,7 @@
 import { neon } from "@neondatabase/serverless";
 
 export async function GET(request: Request) {
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(process.env.DATABASE_URL!);
   const data = await sql`select * from list`;
   return Response.json({
     port:data[0].port
--- src/app/api/updateStun/route.ts
+++ src/app/api/updateStun/route.ts
@@ -3,8 +3,8 @@ import { neon } from "@neondatabase/serverless";
 
 export async function POST(request: Request) {
   const res = await request.json();
-
-  const sql = neon(process.env.DATABASE_URL);
+  
+  const sql = neon(process.env.DATABASE_URL!);
   const data = await sql`UPDATE list SET port = ${res.port} WHERE id = 1`;
   return Response.json({ state: "ok",port:res.port });
 }
